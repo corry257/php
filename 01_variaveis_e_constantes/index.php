adddaadf486ae5aa2c827c9da387af59b3639505
alan-neves <alan.n.ribeiro257@hotmail.com>
2025-09-19 16:54:45 -0300

    new folder

---
+++ 01_variaveis_e_constantes/index.php
@@ -0,0 +1,41 @@
+<?php
+	//Introdução A PHP 
+	
+	//Variaveis e constantes: 
+	$nome = "Alan";
+	$sobrenome = "Neves";
+	const PAIS = "Brasil";
+	
+	//Exercício:
+	$idade = 30;
+	$peso = 100.5;
+	$casado = false;
+	
+	const CURSO = "TI";
+	
+	echo "Muito prazer, meu nome é $nome $sobrenome e moro no " . PAIS . "\n"; 
+
+	echo "Tenho $idade anos, peso $peso kg \n";
+	
+	echo "Estou fazendo o curso de " . CURSO . " e ";
+	
+	if ($casado == true) {
+		echo "Sou casado";
+	}
+	else {
+		echo "não sou casado";
+	}
+	
+	/*
+	Esse exercicio acima deve imprimir:
+		"Muito prazer, meu nome é Alan Neves e moro no Brasil
+		Tenho 30 anos, peso 100.5 kg 
+		Estou fazendo o curso de TI e não sou casado"
+	*/
+	
+	//Para nomear métodos e atributos:
+	$nomeCompletoCliente = "Camel Case";
+	
+	//Para nomear constantes:
+	$telefone_contato_fornecedor = "Snake Case";
+  
